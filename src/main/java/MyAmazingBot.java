05fe457bc6d8d6c912b5d03828c70e82785b27c6
Vladimir S <40244085+triXXXy13@users.noreply.github.com>
2020-11-07 20:19:23 +0300

    Update MyAmazingBot.java

--- src/main/java/MyAmazingBot.java
+++ src/main/java/MyAmazingBot.java
@@ -19,7 +19,41 @@ public class MyAmazingBot extends TelegramLongPollingBot {
              } catch (TelegramApiException e){
                  e.printStackTrace();
              }
-         }
+         }else if (update.hasMessage() && update.getMessage().hasPhoto()) {
+    // Message contains photo
+    // Set variables
+    long chat_id = update.getMessage().getChatId();
+
+    // Array with photo objects with different sizes
+    // We will get the biggest photo from that array
+    List<PhotoSize> photos = update.getMessage().getPhoto();
+    // Know file_id
+    String f_id = photos.stream()
+                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+                    .findFirst()
+                    .orElse(null).getFileId();
+    // Know photo width
+    int f_width = photos.stream()
+                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+                    .findFirst()
+                    .orElse(null).getWidth();
+    // Know photo height
+    int f_height = photos.stream()
+                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+                    .findFirst()
+                    .orElse(null).getHeight();
+    // Set photo caption
+    String caption = "file_id: " + f_id + "\nwidth: " + Integer.toString(f_width) + "\nheight: " + Integer.toString(f_height);
+    SendPhoto msg = new SendPhoto()
+                    .setChatId(chat_id)
+                    .setPhoto(f_id)
+                    .setCaption(caption);
+    try {
+        sendPhoto(msg); // Call method to send the photo with caption
+    } catch (TelegramApiException e) {
+        e.printStackTrace();
+    }
+}
     }
 
     public String getBotUsername() {
