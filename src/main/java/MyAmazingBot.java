cde88699a1401d7722898b0fbd647fd9c419560f
Nemo <hackersaheadddd@gmail.com>
2020-11-13 18:14:58 +0300

    added logging function

--- src/main/java/MyAmazingBot.java
+++ src/main/java/MyAmazingBot.java
@@ -3,57 +3,36 @@ import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class MyAmazingBot extends TelegramLongPollingBot {
 
-
+    @Override
     public void onUpdateReceived(Update update) {
-         if(update.hasMessage() && update.getMessage().hasText()){
-             String message_text = update.getMessage().getText();
-             long chat_id = update.getMessage().getChatId();
-             SendMessage message = new SendMessage()
-                     .setChatId(chat_id)
-                     .setText(message_text);
-             try{
-                 execute(message);
-             } catch (TelegramApiException e){
-                 e.printStackTrace();
-             }
-         }else if (update.hasMessage() && update.getMessage().hasPhoto()) {
-    // Message contains photo
-    // Set variables
-    long chat_id = update.getMessage().getChatId();
 
-    // Array with photo objects with different sizes
-    // We will get the biggest photo from that array
-    List<PhotoSize> photos = update.getMessage().getPhoto();
-    // Know file_id
-    String f_id = photos.stream()
-                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
-                    .findFirst()
-                    .orElse(null).getFileId();
-    // Know photo width
-    int f_width = photos.stream()
-                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
-                    .findFirst()
-                    .orElse(null).getWidth();
-    // Know photo height
-    int f_height = photos.stream()
-                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
-                    .findFirst()
-                    .orElse(null).getHeight();
-    // Set photo caption
-    String caption = "file_id: " + f_id + "\nwidth: " + Integer.toString(f_width) + "\nheight: " + Integer.toString(f_height);
-    SendPhoto msg = new SendPhoto()
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String user_first_name = update.getMessage().getChat().getFirstName();
+            String user_last_name = update.getMessage().getChat().getLastName();
+            String user_username = update.getMessage().getChat().getUserName();
+            long user_id = update.getMessage().getChat().getId();
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            String answer = message_text;
+            SendMessage message = new SendMessage() // Create a message object object
                     .setChatId(chat_id)
-                    .setPhoto(f_id)
-                    .setCaption(caption);
-    try {
-        sendPhoto(msg); // Call method to send the photo with caption
-    } catch (TelegramApiException e) {
-        e.printStackTrace();
-    }
-}
+                    .setText(answer);
+            log(user_first_name, user_last_name, Long.toString(user_id), message_text, answer);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public String getBotUsername() {
@@ -63,4 +42,14 @@ public class MyAmazingBot extends TelegramLongPollingBot {
     public String getBotToken() {
         return "1376500093:AAFNVrrpsqLXOb5zmhvSQGl33fGK_7COxZE";
     }
+    private void log(String first_name, String last_name, String user_id, String txt, String bot_answer) {
+        System.out.println("\n ----------------------------");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+        System.out.println("Message from " + first_name + " " + last_name + ". (id = " + user_id + ") \n Text - " + txt);
+        System.out.println("Bot answer: \n Text - " + bot_answer);
+    }
+
+
 }
